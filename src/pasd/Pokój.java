6dc9b602f6916cd6715779d331b4208f11f725f1
Paweł Jan Czochański <czochanski@gmail.com>
2015-12-01 15:06:23 +0100

    Poprawiłem implementację wyrzucania gości.

--- src/pasd/Pokój.java
+++ src/pasd/Pokój.java
@@ -2,6 +2,7 @@ package pasd;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class Pokój {
 
@@ -23,11 +24,7 @@ public class Pokój {
     }
 
     boolean wyrzuć(Gość g) {
-        if (goście.indexOf(g) != -1) {
-            return false;
-        }
-        goście.remove(g);
-        return true;
+        return goście.remove(g);
     }
 
     public int getPiętro() {
@@ -61,5 +58,41 @@ public class Pokój {
     public void setGoście(List<Gość> goście) {
         this.goście = goście;
     }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 31 * hash + this.piętro;
+        hash = 31 * hash + this.numer;
+        hash = 31 * hash + this.pojemność;
+        hash = 31 * hash + Objects.hashCode(this.goście);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Pokój other = (Pokój) obj;
+        if (this.piętro != other.piętro) {
+            return false;
+        }
+        if (this.numer != other.numer) {
+            return false;
+        }
+        if (this.pojemność != other.pojemność) {
+            return false;
+        }
+        if (!Objects.equals(this.goście, other.goście)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
 
 }
