3f36c156464bce5edb1b10d42205d588ab169228
Paweł Jan Czochański <czochanski@gmail.com>
2015-12-01 15:02:55 +0100

    Dodałem klasę Hotel.

---
+++ src/pasd/Hotel.java
@@ -0,0 +1,40 @@
+package pasd;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Hotel {
+
+    private List<Pokój> pokoje;
+
+    public Hotel() {
+        pokoje = new ArrayList<>();
+    }
+
+    boolean zakwateruj(Gość g) {
+        for (Pokój pokój : pokoje) {
+            if (pokój.zakwateruj(g)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    boolean wyrzuć(Gość g) {
+        for (Pokój pokój : pokoje) {
+            if (pokój.getGoście().remove(g)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public List<Pokój> getPokoje() {
+        return pokoje;
+    }
+
+    public void setPokoje(List<Pokój> pokoje) {
+        this.pokoje = pokoje;
+    }
+
+}
